8cf8cb85cbb1c01c87e6dff2f50ea24996a05f31
jsierra93 <jsierra93@hotmail.com>
2020-11-18 16:48:24 -0500

    Se implementan metodos para Mono

--- src/main/java/co/com/jsierra/reactor/Main.java
+++ src/main/java/co/com/jsierra/reactor/Main.java
@@ -1,7 +1,44 @@
 package co.com.jsierra.reactor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+
 public class Main {
+    private final static Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Demo");
+        MonoZip();
+
+    }
+
+    public static void Mono() {
+        System.out.println("----- MonoZip -------------- ");
+        Mono<String> result = Mono.just("")
+                .map(d -> "spring");
+
+             result.subscribe(
+                val -> log.info("Val {}", val),
+                error -> log.info("Error {}", error),
+                () -> log.info("complete")
+        );
+
+    }
+
+    public static void MonoZip() {
+        System.out.println("----- MonoZip -------------- ");
+        Mono<String> testMono = Mono.just("Demo");
+        Mono<String> testMono2 = Mono.just("Reactor");
+
+        Mono<String> test = Mono.zip(
+                testMono, testMono2
+        ).flatMap(t1 -> {
+            String res = t1.getT1() + "-" + t1.getT2();
+            return Mono.just(res);
+        }).log();
+
+        test.subscribe(
+                val -> log.info("Result {}", val)
+        );
     }
 }
